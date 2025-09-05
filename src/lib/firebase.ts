7ef71b869bdf0ccdd58b3de501bde7e4ef84754e
Debaprasad B <bapidev.BD@gmail.com>
2025-09-05 12:29:08 +0000

    Try fixing this error: `Console Error: [2025-09-05T12:28:37.287Z] @fireb

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,7 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
@@ -15,7 +16,14 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+
+const db = initializeFirestore(app, {
+    localCache: persistentLocalCache({
+        tabManager: persistentMultipleTabManager(),
+    }),
+    experimentalForceLongPolling: true,
+});
+
 const auth = getAuth(app);
 
 export { db, auth };
