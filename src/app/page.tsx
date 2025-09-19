f54f11f26a73623976e01929d9aba278b347d70c
Debaprasad B <bapidev.BD@gmail.com>
2025-09-19 04:36:00 +0000

    realign the margins of this page (_for element <Home>_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { useAuth } from '@/components/auth/AuthProvider';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-full">
+    <div className="container mx-auto p-4 sm:p-6 lg:p-8 flex flex-col h-full">
       <div className="mb-8">
         <h1 className="text-3xl font-bold tracking-tight">Welcome Back!</h1>
         <p className="text-muted-foreground">Here are your available modules.</p>
