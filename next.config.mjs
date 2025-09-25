0753f9b888e75e5241e8f18422277f2d6801feba
Debaprasad B <bapidev.BD@gmail.com>
2025-09-25 05:45:30 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+    ],
   },
 };
 
