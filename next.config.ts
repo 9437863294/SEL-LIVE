b05c7920d7a26c352631d1c6bbd8405f757e15c1
Debaprasad B <bapidev.BD@gmail.com>
2025-09-25 05:25:13 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -34,10 +34,6 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: 'module-hub-uc7tw.appspot.com',
   },
-  allowedDevOrigins: [
-    'https://6000-firebase-studio-1757047573465.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev',
-    'https://9000-firebase-studio-1757047573465.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev',
-  ],
 };
 
 export default nextConfig;
