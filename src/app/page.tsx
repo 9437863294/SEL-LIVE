54d3f1fcfcd986f94db8b2251992be34b6f3f593
Debaprasad B <bapidev.BD@gmail.com>
2025-09-26 04:40:53 +0000

    PROBLEMS ModuleDashboard.tsx Filter (e.g. text, @ Argument of type 'Mod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,10 @@ import { useAuth } from '@/components/auth/AuthProvider';
 export default function Home() {
   const { user } = useAuth();
   return (
-    <div className="container mx-auto p-4 sm:p-6 lg:p-8 flex flex-col">
-      
-      <ModuleDashboard />
-    </div>
+    <main className="flex-1 p-4 sm:p-6 lg:p-8">
+      <div className="container mx-auto h-full">
+        <ModuleDashboard />
+      </div>
+    </main>
   );
 }
