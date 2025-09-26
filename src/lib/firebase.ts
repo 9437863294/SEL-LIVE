906b96b87ca7b95031fb74ecc2301237c4a0f389
Debaprasad B <bapidev.BD@gmail.com>
2025-09-26 05:40:32 +0000

    ariable 'dbl implicitly has type 'any' in some locations where its type

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
-import { getFirestore, initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from "firebase/firestore";
+import { getFirestore, initializeFirestore, persistentLocalCache, persistentMultipleTabManager, type Firestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
@@ -17,7 +17,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
-let db;
+let db: Firestore;
 if (typeof window !== 'undefined') {
   // Client-side execution
   db = initializeFirestore(app, {
