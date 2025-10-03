c3d8246f984be329137842ad169b40c631bf7a3d
Debaprasad B <bapidev.BD@gmail.com>
2025-10-03 06:38:14 +0000

    Try fixing this error: `Runtime Error: Error: ./src/app/globals.css Erro

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,10 @@
+
 import type {Config} from 'tailwindcss';
 
 export default {
   darkMode: ['class'],
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
