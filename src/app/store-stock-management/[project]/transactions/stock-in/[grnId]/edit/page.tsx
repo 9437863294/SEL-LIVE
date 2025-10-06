37e96cd01aeabc7878b08f4d161f749641b44dcb
Debaprasad B <bapidev.BD@gmail.com>
2025-10-06 06:47:16 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/app/store-stock-management/[project]/transactions/stock-in/[grnId]/edit/page.tsx
+++ src/app/store-stock-management/[project]/transactions/stock-in/[grnId]/edit/page.tsx
@@ -4,7 +4,7 @@
 import { useState, useEffect, useRef } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
-import { useForm, useFieldArray } from 'react-hook-form';
+import { useForm, useFieldArray, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import {
