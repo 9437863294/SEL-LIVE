b066644f7f07c6f14416f7589d5d481594e2772f
Debaprasad B <bapidev.BD@gmail.com>
2025-10-23 07:10:10 +0000

    Try fixing this error: `Runtime Error: Error: An unexpected Turbopack er

--- next.config.ts
+++ next.config.ts
@@ -34,19 +34,6 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: 'module-hub-uc7tw.appspot.com',
   },
-  webpack: (config, { isServer }) => {
-    // This is to prevent the Genkit flow files from triggering the Next.js file watcher.
-    // See: https://nextjs.org/docs/app/api-reference/next-config-js/webpack
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: [
-        ...config.watchOptions.ignored,
-        '**/.firebase/**',
-        '**/src/ai/**'
-      ],
-    }
-    return config
-  },
 };
 
 export default nextConfig;
