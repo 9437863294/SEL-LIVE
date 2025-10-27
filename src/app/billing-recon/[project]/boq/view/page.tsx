7bba2f67f22a22c7fe761dc5bfe2bae1bd6aa733
Debaprasad B <bapidev.BD@gmail.com>
2025-10-27 11:52:06 +0000

    JMC Certified Qty AT /billing-recon/madanpur-rampur/boq/view NOT SHOWING

--- src/app/billing-recon/[project]/boq/view/page.tsx
+++ src/app/billing-recon/[project]/boq/view/page.tsx
@@ -33,7 +33,7 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter, DialogClose, DialogTrigger, DialogDescription } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter, DialogClose, DialogDescription } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Checkbox } from '@/components/ui/checkbox';
