c13e154696e95e4c3da919069118c3df7b03594a
Debaprasad B <bapidev.BD@gmail.com>
2025-11-08 10:34:19 +0000

    margin not given (_for element <ModuleDashboard>_)

--- src/components/ModuleDashboard.tsx
+++ src/components/ModuleDashboard.tsx
@@ -105,7 +105,7 @@ export default function ModuleDashboard() {
   }, []);
 
   return (
-    <div className="flex flex-col gap-8 h-full">
+    <div className="flex flex-col gap-8 h-full m-4">
        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6" onDragOver={handleDragOver}>
         {isLoading || authLoading ? (
             Array.from({ length: 5 }).map((_, i) => (
