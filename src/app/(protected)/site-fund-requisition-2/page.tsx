e5dfc447ecdb667f3cbf24fd6fbe605d1bee74c7
Debaprasad B <bapidev.BD@gmail.com>
2025-12-09 11:44:54 +0000

    work on this page margin and padding  (_for element <AppShell>_)

--- src/app/(protected)/site-fund-requisition-2/page.tsx
+++ src/app/(protected)/site-fund-requisition-2/page.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 export default function SiteFundRequisition2Page() {
   return (
-    <div>
+    <div className="p-4 sm:p-6 lg:p-8">
       <h1 className="text-3xl font-bold mb-6">Site Fund Requisition 2</h1>
       <Card>
         <CardHeader>
